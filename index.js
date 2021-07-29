2e784ec6dbdc57719acf6eb406c7cf5ea3badc93
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-29 15:54:26 -0400

    commit tree 5716dd5bcf0c27011364c1769cc7452671e73a2a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+let newHeader = document.body.appendChild(document.createElement("h1"));
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
